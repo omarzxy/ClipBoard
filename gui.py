2547522c776a6b725670918b967a5c9240c11cef
omarzxy <omar-ali1997@hotmail.com>
2019-03-07 22:00:35 -0700

    Added reading from clipboard without formatting and only strings

--- gui.py
+++ gui.py
@@ -1,32 +1,45 @@
-from PyQt5 import QtGui,QtWidgets,QtCore
+from PyQt5.QtWidgets import *
+from AppKit import NSPasteboard, NSArray,NSStringPboardType
 import sys
 
-class Gui(QtWidgets.QMainWindow):
+class Gui(QMainWindow):
     def __init__(self):
         super(Gui, self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("ClipBoard!")
-        #self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
 
-        self.setWindowIcon(QtGui.QIcon("AppIcon29x29.png"))
-
-        extractAction = QtWidgets.QAction("&GET TO THE CHOPPAH!!!", self)
+        self._main = QWidget()
+        self.setCentralWidget(self._main)
+        self.Layout = QVBoxLayout(self._main)
+        self.createLabelsForClipBoardContents()
+        extractAction = QAction("&GET TO THE CHOPPAH!!!", self)
         extractAction.setShortcut("Ctrl+Q")
         extractAction.setStatusTip('Leave The App')
         extractAction.triggered.connect(self.close_application)
-
+        QAction("FFA",self)
         self.statusBar()
         self.home()
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('&File')
         fileMenu.addAction(extractAction)
+    def createLabelsForClipBoardContents(self):
+        horizontalLayout = QHBoxLayout()
+
+        timeLabel = QLabel("time")
+        pb = NSPasteboard.generalPasteboard()
+        textInClipBoard = pb.stringForType_(NSStringPboardType)
+        textInClipBoardLabel = QLabel(textInClipBoard)
+        horizontalLayout.addWidget(timeLabel)
+        horizontalLayout.addWidget(textInClipBoardLabel)
+        self.Layout.addLayout(horizontalLayout)
     def home(self):
-        btn = QtWidgets.QPushButton("Quit", self)
+        btn = QPushButton("Quit", self)
         btn.clicked.connect(self.close_application)
         btn.resize(btn.minimumSizeHint())
-        btn.move(0,0)
+        btn.move(400,300)
         self.show()
+
     def close_application(self):
 
         print("whooaaaa so custom!!!")
@@ -34,6 +47,7 @@ class Gui(QtWidgets.QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    GUI = Gui()
-    sys.exit(app.exec_())
+    qapp = QApplication(sys.argv)
+    app = Gui()
+    app.show()
+    sys.exit(qapp.exec_())
